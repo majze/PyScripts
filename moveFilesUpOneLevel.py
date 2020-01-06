f8ca87123a6d35f5b80d4beb00fff80fe365dd55
Don Spreder <dspreder@doxim.com>
2020-01-06 15:10:41 -0500

    Native OS friendly & supports optional arguments

--- moveFilesUpOneLevel.py
+++ moveFilesUpOneLevel.py
@@ -1,85 +1,93 @@
-# Move all contents of subdirectories up to their parent directory.
-# For example, ~/someDir/folder1 , ~/someDir/folder2 , ... , ~/someDir/folderN 
-# and all of the contents of the sub directories brought up to ~/someDir/
-# IMPORTANT!!! This script assumes ~/someDir/ ONLY contains subdirectories.
-# After moving all files, ~/someDir/ only includes this script and the sub directories
+""" Author: MajZe (D. Spreder) | Language: Python 3.7.4 | Last Edited: Jan 6, 2020
+# This script moves all contents of subdirectories up to their parent directory.
+  For example, ~/someDir/folder1 , ~/someDir/folder2 , ... , ~/someDir/folderN ,
+  and all of the contents of the sub directories brought up to ~/someDir/
+# IMPORTANT! Start this script in the top level directory.
+# Optional arguments:
+    --path full_path
+        String - Replace 'full_path' with FULL path to top level directory of ~/someDir
+    --verbose
+        Boolean - No additional input required
+    --rm
+        Boolean - No additional input required
+# See function argv_init() for more information on arguments
+"""
 
-# Arguments must be entered in correct order as seen below:
-# optionalArg1=parentPath >> path: points the script towards the parent path instead of running locally
-# optionalArg2=removeEmpties >> Boolean: { {True,true,1}, {False,false,0} } for removeEmpties logic
-# If using optionalArg2, you MUST use optionalArg1 first. Working on modularizing this.
-
-import shutil
+import argparse
 import os
+import shutil
 import sys
+from colorama import init, Fore
+init()
 
 def main():
+    # argument parser
+    parser = argparse.ArgumentParser(description='Optional flags to control script')
+    argv_init(parser)
+    args = parser.parse_args()
+    
+    # Original/starting directory
+    if args.path:
+        origin = args.path
+    else:
+        origin = os.path.dirname(os.path.realpath(__file__))
+        print("\nNo path specified - Default execution path: ", origin)
+        # Confirm no path argument if using script file locally
+        askConfirm = input(f"{Fore.CYAN}Confirm (y/n): {Fore.RESET}")
+        if askConfirm == "y" or askConfirm == "Y":
+            print("Path confirmed, executing...\n")
+        elif askConfirm == "n" or askConfirm == "N":
+            print(f"{Fore.RED}** Job cancelled by user **{Fore.RESET}\n")
+            sys.exit()
+        else:
+            print(f"{Fore.YELLOW}Interpreting vague answer as no, stopping script!{Fore.RESET}\n")
+            raise Exception(f"{Fore.RED}Unexpected user input confirming execution path{Fore.RESET}\n")
+    
+    # Log path and start of script
+    print(f"\nStarting Job in: {origin}")
 
-	# Get and parse optional args
-	# Original/starting directory
-	origin = ""
-	if len(sys.argv) > 1:
-		if ("\\" in sys.argv[1]) or ("/" in sys.argv[1]):
-			origin = sys.argv[1]
-		else:
-			print("\nWARNING: Bad path or missing path. Start script in default location?")
-			origin = os.path.dirname(os.path.realpath(__file__))
-			print("Default location: ", origin)
-			
-			# Confirm no path argument if using script file locally
-			askConfirm = input("Enter y/n: ")
-			if askConfirm == "y" or askConfirm == "Y":
-				print("Confirmed path, starting job\n")
-			elif askConfirm == "n" or askConfirm == "N":
-				print("** Job cancelled by user **\n")
-				sys.exit()
-			else:
-				print("\nInterpreting vague answer as no, stopping script\n")
-				raise Exception("** Unexpected input for askConfirm **\n")
-	else:
-		origin = os.path.dirname(os.path.realpath(__file__))
-	
-	# Confirm and log optionalArg1=parentPath
-	print("\n** Starting Job **\nLocation: {}".format(origin))
-	
-	# Remove empty subdirectories when finished?
-	removeEmpties = True
-	if len(sys.argv) > 2:
-		commandArgs = [['true', '1'], ['false', '0']]
-		if sys.argv[2].casefold() in commandArgs[0]:
-			removeEmpties = True
-			print("Remove subdirectories after moving files: True\n")
-		elif sys.argv[2].casefold() in commandArgs[1]:
-			removeEmpties = False
-			print("Remove subdirectories after moving files: False\n")
-		else:
-			print("Bad input for optionalArg2=removeEmpties, exiting\n")
-			sys.exit()
-	
-	# Current working directory
-	cwd = os.getcwd()
+    # Get list of all subdirectories and log paths
+    dirList = [dir for dir in os.listdir(origin) if os.path.isdir(join(origin, dir)) and dir != __file__[2:]]
+    print(f"Directories: {dirList}\n")
+    
+    # Parse through subdirectories
+    for dir in dirList:
+        # Current working directory
+        cwd = join(origin, dir)
+        if (args.verbose):
+            print(f"Parsing {dir}/ ", '='*12)
+        
+        # Get list of and parse through files in current subdirectory
+        fileList = os.listdir(cwd)
+        for file in fileList:
+            if (args.verbose):
+                print(f"moving {file}")
+            shutil.move(join(cwd, file), origin)
 
-	# Get list of all subdirectories
-	dirList = [dir for dir in os.listdir(origin) if dir != __file__[2:]]
+    # Remove empty directories when done, if removeEmpties == True
+    if (args.rm):
+        for dir in dirList:
+            if (args.verbose):
+                print(f"Removing {dir}")
+            os.rmdir(os.path.join(origin, dir))
+        print("** Removed empty directories **\n")
+                
+    print("** Operation complete **\n")
 
-	# Parse through subdirectories
-	for dir in dirList:
-		cwd = os.path.join(origin, dir)
-		print("Parsing", dir + "/", " ====================")
-		
-		# Get list of and parse through files in current subdirectory
-		fileList = os.listdir(cwd)
-		for file in fileList:
-			print("moving ", file)
-			shutil.move(os.path.join(cwd, file), origin)
 
-	# Remove empty directories when done, if removeEmpties == True
-	if removeEmpties:
-		for dir in dirList:
-			os.rmdir(os.path.join(origin, dir))
-		print("** Removed empty directories **\n")
-				
-		print("** Operation complete! **\n")
+def join(path1, path2):
+    """Shorthand function for os.path.join()"""
+    return os.path.join(path1, path2)
+
+
+def argv_init(parser):
+    parser.add_argument('--path',
+                        help='Full path to excute script in')
+    parser.add_argument('--verbose', action='store_true',
+                        help='Outputs every action to the terminal')
+    parser.add_argument('--rm', action='store_true',
+                        help='Removes empty directories after moving files')
+
 
 if __name__ == "__main__":
-	main()
+    main()
