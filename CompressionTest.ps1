1fad0a220186f0309c6a3338493d831c3af8afc5
Donald <22397029+majze@users.noreply.github.com>
2020-06-11 17:21:44 -0400

    Powershell 7z algo test to determine size/time

--- CompressionTest.ps1
+++ CompressionTest.ps1
@@ -1,9 +1,17 @@
-﻿# Globals - Modify these lines to match your environment's paths
-$WorkPath = "D:\Inspire\zipTest"
-$contentsFolder = "$WorkPath\ArchivePDF"
+# Globals - Modify these lines to match your environment's paths
 $zipPath = "C:\Program Files\7-Zip\7z.exe"
+$WorkPath = "D:\zipTest"
+$contentsDirectory = "ArchivePDF"
+$logFile = "zipLog.txt"
+$zipExt = ".zip"
+
+# Input Arguments
+if (!($zipExt.StartsWith('.'))) {
+    $zipExt = '.' + $zipExt
+}
 
 # Globals - For measuring average time of compression algorithm
+[System.Collections.ArrayList]$mx3 = @()
 [System.Collections.ArrayList]$mx5 = @()
 [System.Collections.ArrayList]$mx7 = @()
 [System.Collections.ArrayList]$mx9 = @()
@@ -11,19 +19,20 @@ $zipPath = "C:\Program Files\7-Zip\7z.exe"
 
 # Check if paths exists and remove old log file
 if (!(Test-Path -Path $WorkPath))
-    { Throw "$WorkPath does not exist" }
-if (!(Test-Path -Path $contentsFolder))
-    { Throw "$contentsFolder does not exist" }
+    { Throw "Directory $WorkPath does not exist" }
+if (!(Test-Path -Path (Join-Path $WorkPath $contentsDirectory)))
+    { Throw "Directory $contentsDirectory does not exist" }
 if (!(Test-Path -Path $zipPath))
-    { Throw "$zipPath does not exist" }
-if (Test-Path -Path "$WorkPath\zipLog.txt")
-    { Remove-Item "$WorkPath\zipLog.txt" }
+    { Throw "Executable $zipPath does not exist" }
+if (Test-Path -Path (Join-Path $WorkPath $logFile))
+    { Remove-Item (Join-Path $WorkPath $logFile) }
 
 # Function - Format and display the TimeSpan value.
 function writeTime($sw, [string]$zip) {
     $elapsedTime = $sw.Minutes*60 + $sw.Seconds + $sw.Milliseconds/1000
-    Write-Output "$zip $elapsedTime seconds" | Out-File -FilePath "$WorkPath\zipLog.txt" -Append
+    Write-Output "$zip $elapsedTime seconds" | Out-File -FilePath (Join-Path $WorkPath $logFile) -Append
     switch($zip) {
+        "-mx=3" { $mx3.Add($elapsedTime) }
         "-mx=5" { $mx5.Add($elapsedTime) }
         "-mx=7" { $mx7.Add($elapsedTime) }
         "-mx=9" { $mx9.Add($elapsedTime) }
@@ -43,7 +52,7 @@ function getAvg($list) {
 # Function - Cleanup zip archives when beginning each iteration of the main loop
 function cleanupZips() {
     Set-Location $WorkPath
-    Get-ChildItem -Filter *.zip -File -Recurse | ForEach-Object {
+    Get-ChildItem -Filter *$zipExt -File -Recurse | ForEach-Object {
         Remove-Item $_.FullName -Force -Recurse 
     }
 }
@@ -51,15 +60,16 @@ function cleanupZips() {
 # Function - Start a stopwatch and time the zip process using the provided compression level
 function runZipProcess($compressionLevel) {
     $sw = [Diagnostics.Stopwatch]::StartNew()
-    Start-Process $zipPath -ArgumentList "a -tzip $compressionLevel .\$compressionLevel.zip .\ArchivePDF\*" -wait
+    Start-Process $zipPath -ArgumentList "a -tzip $compressionLevel .\$compressionLevel$zipExt .\$contentsDirectory\*" -wait
     $sw.Stop()
     writeTime $sw.Elapsed $compressionLevel
 }
 
 # Main Loop - Goes over 5 iterations by default for a more accurate average.
-For ([int]$i = 0; $i -lt 5; $i++) {
+For ([int]$i = 0; $i -lt 3; $i++) {
     Write-Host "Running zip pass #$($i + 1)"
     cleanupZips -wait
+    runZipProcess "-mx=3" | Out-null
     runZipProcess "-mx=5" | Out-null
     runZipProcess "-mx=7" | Out-null
     runZipProcess "-mx=9" | Out-null
@@ -67,8 +77,9 @@ For ([int]$i = 0; $i -lt 5; $i++) {
 }
 
 # Get average times and output to log
-Write-Output "-mx=5 $(getAvg $mx5) seconds (avg)" | Out-File -FilePath ".\zipLog.txt" -Append
-Write-Output "-mx=7 $(getAvg $mx7) seconds (avg)" | Out-File -FilePath ".\zipLog.txt" -Append
-Write-Output "-mx=9 $(getAvg $mx9) seconds (avg)" | Out-File -FilePath ".\zipLog.txt" -Append
-Write-Output "-m0=LZMA $(getAvg $LZMA) seconds (avg)" | Out-File -FilePath ".\zipLog.txt" -Append
-Write-Host "Job complete - see $WorkPath\zipLog.txt"
+Write-Output "-mx=3 $(getAvg $mx3) seconds (avg)" | Out-File -FilePath ".\$logFile" -Append
+Write-Output "-mx=5 $(getAvg $mx5) seconds (avg)" | Out-File -FilePath ".\$logFile" -Append
+Write-Output "-mx=7 $(getAvg $mx7) seconds (avg)" | Out-File -FilePath ".\$logFile" -Append
+Write-Output "-mx=9 $(getAvg $mx9) seconds (avg)" | Out-File -FilePath ".\$logFile" -Append
+Write-Output "-m0=LZMA $(getAvg $LZMA) seconds (avg)" | Out-File -FilePath ".\$logFile" -Append
+Write-Host "Job complete - see $WorkPath\$logFile"
